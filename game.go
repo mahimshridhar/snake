a98d4412039567ee284836bc006d12ff3b0c0c0a
mahimshridhar <mahimshridhar@gmail.com>
2021-08-21 19:50:42 +0530

    added basic entities

---
+++ game.go
@@ -0,0 +1,7 @@
+package main
+
+type Game struct {
+	score      int
+	isGameOver bool
+	playground *Playground
+}
---
+++ playground.go
@@ -0,0 +1,6 @@
+package main
+
+type Playground struct {
+	width  int
+	height int
+}
